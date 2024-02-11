e8ecdf31b47ecc57c059ba0cd70e69cfdc228302
brash <s25070@pjwstk.edu.pl>
2024-02-11 19:11:18 +0100

    Initial Commit

---
+++ githubRepositories/src/main/java/com/atipera/githubRepositories/GithubRepositoriesApplication.java
@@ -0,0 +1,13 @@
+package com.atipera.githubRepositories;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubRepositoriesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GithubRepositoriesApplication.class, args);
+	}
+
+}
